eec9c3800d38a06d07becedba7ced3580114b055
Axel de Gier <532600@student.glu.nl>
2021-06-30 14:41:23 +0200

    online

--- code/src/App.js
+++ code/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 export function App() {
+
+if (!localStorage.getItem('username')) return <LoginForm />;
+	  
 	return (
 		<ChatEngine
 			height='100vh'
-			userName='Admin'
-			userSecret='Admin123'
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
 			projectID='27aa9843-f2b8-4a11-8640-85ef2cc4b1c3'
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
